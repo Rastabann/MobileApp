5d54d9ac5cb322097747c2327834f9a1bcae24fa
Rastabann <alexis.godin1207@gmail.com>
2023-05-06 14:09:45 +0200

    firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCVobVTynVKhIlyU_jPir_Pfe1OHXfbSxk",
+  authDomain: "guardaxapp.firebaseapp.com",
+  projectId: "guardaxapp",
+  storageBucket: "guardaxapp.appspot.com",
+  messagingSenderId: "294433421757",
+  appId: "1:294433421757:android:59e68446c58cc521a97ad4",
+};
+
+export default firebaseConfig;
